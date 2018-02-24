d71953ae49aec801591289ccc2d95123cce49f35
Julian v.d Berkmortel <julianvdberkmortel@outlook.com>
2018-02-24 21:46:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Learning Vue.js
+A simple project to push things to that I made while learning Vue.js
---
+++ app.js
@@ -0,0 +1,36 @@
+var vue = new Vue({
+    el: "#app",
+    data: {
+        name: "",
+        email: "",
+        message: "",
+        messages: [],
+    },
+    computed: {
+        charactersLeft: function() {
+            return 140 - this.message.length;
+        },
+        outOfCharacters: function() {
+            return this.charactersLeft < 0;
+        },
+        canSubmit: function() {
+            return this.name.length > 0 && this.email.length > 0 && this.message.length > 0;
+        }
+    },
+    methods: {
+        onSubmit: function() {
+            if(this.canSubmit) {
+                this.messages.unshift({
+                    name: this.name,
+                    email: this.email,
+                    message: this.message,
+                    date: new Date().toUTCString()
+                });
+
+                this.name = '';
+                this.email = '';
+                this.message = '';
+            }
+        }
+    }
+});
