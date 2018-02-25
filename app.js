b893fdf023e977071b66743d90403af960092253
Julian v.d Berkmortel <julianvdberkmortel@outlook.com>
2018-02-25 02:44:10 +0100

    Add two components to represent posts

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+Vue.component('posts', {
+    template: `
+        <ul class="list-group">
+            <slot></slot>
+        </ul>
+    `
+});
+
+Vue.component('post', {
+    props: ['name', 'message', 'email', 'date'],
+    template: `
+        <li class="list-group-item card post">
+            <h5 class="card-title">{{ name }}</h5>
+            <p class="card-text">{{ message }}</p>
+            <a :href="\`mailto:$\{email\}\`" class="btn btn-primary">Send Email</a>
+            <p class="post-date">{{ date }}</p>
+        </li>
+    `
+});
+
 var vue = new Vue({
     el: "#app",
     data: {
@@ -21,6 +41,7 @@ var vue = new Vue({
         onSubmit: function() {
             if(this.canSubmit) {
                 this.posts.unshift({
+                    id: this.posts.length,
                     name: this.name,
                     email: this.email,
                     message: this.message,
